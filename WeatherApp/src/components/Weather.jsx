a56a1171fc261a48cca8cab849b96ebcecb12fb3
Andrew Teece <andrewteece@gmail.com>
2024-07-20 17:19:57 -0500

    Weather.jsx updated

--- WeatherApp/src/components/Weather.jsx
+++ WeatherApp/src/components/Weather.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
+
 //import Autocomplete from 'react-google-autocomplete';
 import styles from './Weather.module.css';
+import { WEATHER_COORDS_URL } from '../constants';
+import WeatherCard from './WeatherCard';
+
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState({});
---
+++ WeatherApp/src/utils/index.js
@@ -0,0 +1,11 @@
+export const getFormattedDate = () => {
+  const currentDate = new Date();
+  const options = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+  return currentDate.toLocaleString('en-US', options);
+};
